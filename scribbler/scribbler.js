0a007d2fa184a39275c8fcde32a3e2b1d5ee1d91
cameron-bret <c.bret@whiskerhead.com>
2019-03-08 00:17:57 -0500

    updated terminal window on landing page

--- scribbler/scribbler.js
+++ scribbler/scribbler.js
@@ -12,16 +12,17 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
+  var txt = `cbretnichols
+            
+            ###skills
 
-            ###todo for new year dinner party
-
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
-  var speed = 60;
+            - javascript
+            - react
+            - express
+            - python
+            - django
+            - c.bret@whiskerhead.com (404)519-8662`;
+  var speed = 30;
 
   function typeItOut () {
     if (i < txt.length) {
